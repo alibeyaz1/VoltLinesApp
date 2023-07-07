bf80d16a92579b2963043f06cdc9f630b7c5dc6a
Ali Beyaz <ali.beyaz@stu.bilgi.edu.tr>
2023-07-08 00:08:58 +0300

    Create Folder Structure and Assets Added

---
+++ voltlines-case-study/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Extensions.swift
+//  voltlines-case-study
+//
+//  Created by Ali Beyaz on 8.07.2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let clrBlue = UIColor(red: 0.09, green: 0.14, blue: 0.67, alpha: 1.00)
+    static let clrDarkGrey = UIColor(red: 0.22, green: 0.22, blue: 0.22, alpha: 1.00)
+    
+
+
+}
--- voltlines-case-study/Scene/ViewController.swift
+++ voltlines-case-study/Scene/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        
     }
-
-
+    func setupUI(){
+        
+        
+    }
+    
+    
 }
+
 
